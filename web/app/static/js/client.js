1096942ebd9159e53cf331ed8afb19fbe8af9fe7
JavierJia <jianfeng.jia@gmail.com>
2013-06-08 18:56:13 -0700

    it works~

--- web/app/static/js/client.js
+++ web/app/static/js/client.js
@@ -1,7 +1,10 @@
 
 $(function() {
-    $("#datepicker").datepicker();
-  });
+    $("#datepicker").datepicker({ 
+    dateFormat: 'yy-mm-dd',
+    });
+    $("#datepicker").val($.datepicker.formatDate("yy-mm-dd", new Date()));
+});
 
 Client = (function(){
     function Client(){
@@ -40,41 +43,4 @@ Client = (function(){
 }
 )();
 
-
-// function loadBuildingList(){ 
-//     // var ws = new WebSocket('ws://' + window.location.host + '/client');
-//     // ws.onopen = function() {
-//     //    ws.send("Hello, world");
-//     // };
-//     // ws.onmessage = function (evt) {
-//     //    alert(evt.data);
-//     // };   
-//     $.ajax({
-//             url: "http://tomato.ics.uci.edu:3000/test",
-//             type:"GET",
-//             dataType:"json",
-//             data:"",
-//             complete:function(resp){ 
-                                
-//                 var txt = resp.responseText;
-//                 //json format
-//                 var json = $.parseJSON(txt);
-                                
-//                 //jquery dom model processing
-//                 var items = json.results[0];
-//                 var $template = $("#template");
-//                 for(var i = 1;i<items.length && i < 25;i++){
-//                     var $tr = $template.find("tr[id='tempRow']").clone();
-//                     $tr.get(0).id="t" + i;
-//                     var x = eval('(' + items[i] + ')');
-//                     $tr.find(".buildid").html(x.buildid);
-//                     $tr.find(".fullname").html(x.fullname);
-//                     $tr.find(".abbr").html(x.abbr);
-//                     $template.append($tr);
-//                 }
-
-//                 $template.show();
-//             }
-//         })
-// }
 
--- web/app/static/js/list.js
+++ web/app/static/js/list.js
@@ -19,7 +19,45 @@ List = (function(){
           // }
           // if (obj['user'] == this.username) msg.find('.user').addClass('self');
           // this.container.find('ul').append(msg.show());
+
 	}
 
 	return List;
 })();
+
+// function loadBuildingList(){ 
+//     // var ws = new WebSocket('ws://' + window.location.host + '/client');
+//     // ws.onopen = function() {
+//     //    ws.send("Hello, world");
+//     // };
+//     // ws.onmessage = function (evt) {
+//     //    alert(evt.data);
+//     // };   
+//     $.ajax({
+//             url: "http://tomato.ics.uci.edu:3000/test",
+//             type:"GET",
+//             dataType:"json",
+//             data:"",
+//             complete:function(resp){ 
+                                
+//                 var txt = resp.responseText;
+//                 //json format
+//                 var json = $.parseJSON(txt);
+                                
+//                 //jquery dom model processing
+//                 var items = json.results[0];
+//                 var $template = $("#template");
+//                 for(var i = 1;i<items.length && i < 25;i++){
+//                     var $tr = $template.find("tr[id='tempRow']").clone();
+//                     $tr.get(0).id="t" + i;
+//                     var x = eval('(' + items[i] + ')');
+//                     $tr.find(".buildid").html(x.buildid);
+//                     $tr.find(".fullname").html(x.fullname);
+//                     $tr.find(".abbr").html(x.abbr);
+//                     $template.append($tr);
+//                 }
+
+//                 $template.show();
+//             }
+//         })
+// }
